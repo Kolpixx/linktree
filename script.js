27e639da58f1a94512c1f93238b219c181c63717
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2025-06-16 17:52:39 +0200

    toggle musicPlaying & change svg on catch

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 let musicPlaying = true;
 const audio = new Audio("https://raw.githubusercontent.com/Kolpixx/linktree/refs/heads/main/resources/background_jazz.mp3");
+const headphoneOnSvg = document.getElementById("headphone-on");
+const headphoneOffSvg = document.getElementById("headphone-off");
 
 document.addEventListener('DOMContentLoaded', () => {
     audio.play()
-        .catch(err => console.log(err))
+        .catch(err => {
+            console.log(err);
+            headphoneOnSvg.style.display = "none";
+            headphoneOffSvg.style.display = "block";
+            musicPlaying = false;
+        })
     audio.loop = true;
     audio.volume = 0.2;
 })
 
 function toggleMusic() {
-    const headphoneOnSvg = document.getElementById("headphone-on");
-    const headphoneOffSvg = document.getElementById("headphone-off");
-
     if (musicPlaying) {
         audio.pause();
         headphoneOnSvg.style.display = "none";
